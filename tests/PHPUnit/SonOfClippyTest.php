a0998a946e6a9b853fe123f002f50dbea935faba
Steve Grunwell <stevegrunwell@gmail.com>
2017-03-04 11:02:25 -0500

    Introduce a composer.json file and some test scaffolding

---
+++ tests/PHPUnit/SonOfClippyTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tests for the main plugin file.
+ *
+ * @package SonOfClippy
+ * @author  Steve Grunwell
+ */
+
+namespace SonOfClippy;
+
+use WP_Mock as M;
+
+class SonOfClippyTest extends TestCase {
+
+}
---
+++ tests/test-tools/TestCase.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Base PHPUnit test case.
+ *
+ * @package SonOfClippy
+ * @author  Steve Grunwell
+ */
+
+namespace SonOfClippy;
+
+class TestCase extends \WP_Mock\Tools\TestCase {
+
+	public function run( \PHPUnit_Framework_TestResult $result = null ) {
+		$this->setPreserveGlobalState( false );
+		return parent::run( $result );
+	}
+
+	public function assertActionsCalled() {
+		$actions_not_added = $expected_actions = 0;
+		try {
+			WP_Mock::assertActionsCalled();
+		} catch ( \Exception $e ) {
+			$actions_not_added = 1;
+			$expected_actions  = $e->getMessage();
+		}
+		$this->assertEmpty( $actions_not_added, $expected_actions );
+	}
+
+	/**
+	 * Define constants after requires/includes
+	 *
+	 * See http://kpayne.me/2012/07/02/phpunit-process-isolation-and-constant-already-defined/
+	 * for more details
+	 *
+	 * @param \Text_Template $template
+	 */
+	public function prepareTemplate( \Text_Template $template ) {
+		$template->setVar( [
+			'globals' => '$GLOBALS[\'__PHPUNIT_BOOTSTRAP\'] = \'' . $GLOBALS['__PHPUNIT_BOOTSTRAP'] . '\';',
+		] );
+		parent::prepareTemplate( $template );
+	}
+}
